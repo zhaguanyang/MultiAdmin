36618e105ed9312bd85ed210442000b7cfaa382d
Misaka_ZeroTwo <admin@killla.space>
2022-06-09 21:10:02 +0800

    Adds Port to Titlebar

--- MultiAdmin/Features/TitleBar.cs
+++ MultiAdmin/Features/TitleBar.cs
@@ -59,15 +59,16 @@ namespace MultiAdmin.Features
 			{
 				titleBar.Add($"Config: {Server.serverId}");
 			}
+			
+			if (Server.IsGameProcessRunning)
+			{
+				titleBar.Add($"Port: {Server.Port}");
+				titleBar.Add($"PID: {ServerProcessId}");
+			}
 
 			if (Server.SessionSocket != null)
 			{
 				titleBar.Add($"Console Port: {Server.SessionSocket.Port}");
-			}
-
-			if (Server.IsGameProcessRunning)
-			{
-				titleBar.Add($"PID: {ServerProcessId}");
 			}
 
 			try
